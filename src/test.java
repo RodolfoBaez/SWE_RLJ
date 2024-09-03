44f16f5192dda245f23fa61827bb151457c1b874
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-09-03 18:21:50 -0400

    Update test.java with checkStyle error

--- src/test.java
+++ src/test.java
@@ -1,15 +1,22 @@
-Public Class test{
-  public static void main(String args[]){
-      int m = 60;
-      int n = 24;
-      int r = 0;
-      int time complex;
-    
-      System.out.print("the GCD is " + m + "the program ran for " + complex);
-      while( n != 0 ){
-        r = m % n;
-        m = n;
-        n = r;
-        complex++;
+
+public class myClass {
+    public static void main(String[] args) {
+        int m = 60;
+        int n = 24;
+        GcdMachine(m,n);
     }
+   //checkstyle error typeName isnt correct not pascalCase
+   public static void GcdMachine(int m, int n){
+     int r = 0;
+     int timeComplex = 0;
+
+     while (n != 0) {
+            r = m % n;
+            m = n;
+            n = r;
+            timeComplex++;
+        }
+     System.out.println("The GCD is " + m + " and the program ran for " + timeComplex + " iterations.");
+
+   }
 }

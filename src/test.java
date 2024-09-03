7273de63585da4473102bf52a3ccb210ece59357
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-09-03 18:07:37 -0400

    Create test.java
    testing java

---
+++ src/test.java
@@ -0,0 +1,15 @@
+Public Class test{
+  public static void main(String args[]){
+      int m = 60;
+      int n = 24;
+      int r = 0;
+      int time complex;
+    
+      System.out.print("the GCD is " + m + "the program ran for " + complex);
+      while( n != 0 ){
+        r = m % n;
+        m = n;
+        n = r;
+        complex++;
+    }
+}

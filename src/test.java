115596c9968ff7ca157c0525347a562cf5768e23
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-09-05 14:10:52 -0400

    Update test.java
    uppercase the class name, also made the method name first letter undercase

--- src/test.java
+++ src/test.java
@@ -1,12 +1,12 @@
 
-public class test{
+public class Test{
   public static void main(String args[]){
       int m = 60324234;
       int n = 24123412;
       gcdMachine(m,n);
   } 
   // checkstyle error typeName error G should be lowercase
-  public static void GcdMachine(int m, int n){
+  public static void gcdMachine(int m, int n){
         int r = 0;
         int timeComplex = 0;
         

d1d7b3942af2b8c6824be0ace82d0f9ec61a9e33
LourisA <147088420+Loura1928@users.noreply.github.com>
2024-09-16 00:14:25 -0400

    Attempt to align code with checkstyle

--- src/network/api/NetworkBoundAPI.java
+++ src/network/api/NetworkBoundAPI.java
@@ -1,16 +1,16 @@
 package network.api;
 
 public class NetworkBoundAPI {
-public void prototype (UserInput UI, ComputeE CE) {
+	public void prototype (UserInput uI, ComputeE cE) {
 		
 		
 		//reads input from user 
-		UserInput input = UI.getInput();
-		char delimiter = UI.getdelimiter();
+		UserInput input = uI.getInput();
+		char delimiter = uI.getdelimiter();
 		
 		
 		//reads out put from compute engine 
-		ComputeE output = CE.getOutput(input,delimiter);
+		ComputeE output = cE.getOutput(input,delimiter);
 		
 		
 	}

febf32978f0da3c67ff5ee171a1e95c4756a160b
LourisA <147088420+Loura1928@users.noreply.github.com>
2024-09-20 13:19:10 -0400

    Created smoke test for Network Bound Api

---
+++ src/NetworkApiImpl/NetworkApiImpl.java
@@ -0,0 +1,23 @@
+package NetworkApiImpl;
+import network.api.NetworkBoundAPI;
+
+public class NetworkApiImpl {
+	
+	private NetworkBoundAPI networkBoundAPI ; 
+	
+	
+	public NetworkApiImpl(NetworkBoundAPI networkBoundAPI) {
+		this.networkBoundAPI = networkBoundAPI; 
+	}
+	
+	// these should be integers??
+	
+	public String getUserInput (String getInput) {
+		return "";
+	}
+	
+	public String getCeOutput (String getOutput) {
+		return "";
+	}
+	
+}
---
+++ test/testNetworkAPI/TestNetworkAPI.java
@@ -0,0 +1,39 @@
+package testNetworkAPI;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+
+import NetworkApiImpl.NetworkApiImpl;
+import network.api.NetworkBoundAPI;
+
+
+
+public class TestNetworkAPI {
+
+	
+	private NetworkBoundAPI mockApi;
+	private NetworkApiImpl networkApi = new NetworkApiImpl(mockApi); 
+
+	
+	
+
+	// make mock network api object so i can call real one 
+	
+	@Test
+	public void testInput() {
+		String result = networkApi.getUserInput("Test Input");
+		Assertions.assertEquals(result, "Input recived");
+	}
+	
+	@Test 
+	public void testOutput() {
+		String result = networkApi.getCeOutput("Test Output");
+		Assertions.assertEquals(result, "Output given");
+				
+	}
+	
+}
+

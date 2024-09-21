7f3237c2c301164e8ad5fbb077a03252ab5a055d
LourisA <147088420+Loura1928@users.noreply.github.com>
2024-09-21 17:39:52 -0400

    fixed package name for test.netowkr

--- test/test/network/TestNetworkAPI.java
+++ test/test/network/TestNetworkAPI.java
@@ -1,4 +1,4 @@
-package testNetworkAPI;
+package test.network;
 
 import static org.junit.Assert.fail;
 import org.junit.Test;
@@ -7,32 +7,31 @@ import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 
 import network.impl.NetworkApiImpl;
-
 import network.api.NetworkBoundAPI;
 
 
 
-public class TestNetworkAPI{
+public class TestNetworkAPI {
+
 
 	private NetworkApiImpl networkApi = new NetworkApiImpl((Mockito.mock(NetworkBoundAPI.class))); 
 
-	
-	
+
+
 
 	// make mock network api object so i can call real one 
-	
+
 	@Test
 	public void testInput() {
 		String result = networkApi.getUserInput("Test Input");
 		Assertions.assertEquals(result, "Input recived");
 	}
-	
+
 	@Test 
 	public void testOutput() {
 		String result = networkApi.getCeOutput("Test Output");
 		Assertions.assertEquals(result, "Output given");
-				
-	}
-	
-}
 
+	}
+
+}

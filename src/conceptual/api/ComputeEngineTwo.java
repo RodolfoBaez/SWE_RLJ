d801db1428b85a5606cd7aae154ad6f114de063f
RodolfoBaez <47578524+RodolfoBaez@users.noreply.github.com>
2024-09-21 20:59:48 -0400

    Update ComputeEngineTwo.java

--- src/conceptual/api/ComputeEngineTwo.java
+++ src/conceptual/api/ComputeEngineTwo.java
@@ -3,7 +3,8 @@ package conceptual.api;
 public class ComputeEngineTwo implements ComputeEngineTwoInterface<String, Integer> {
     
     public Integer performComputation(String inputData) {
-        // Implement the actual computation logic here
-        return inputData.length();//returning int 
 
-}
+    	return inputData.length(); // returning integer
+    } 
+
+} 

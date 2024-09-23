91a71bca336ea448d012ca4fa8d756970f350e79
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-09-22 23:44:02 -0400

    Update ComputationalAPI.java

--- src/conceptual/api/ComputationalAPI.java
+++ src/conceptual/api/ComputationalAPI.java
@@ -2,40 +2,39 @@ package conceptual.api;
 
 public class ComputationalAPI implements ComputationalAPIInterface {
 
+	private ComputeEngine computeEngine;
+	private ComputeEngineTwo computeEngineTwo;
 
-    private ComputeEngine computeEngine;        
-    private ComputeEngineTwo computeEngineTwo;  
+	// Constructor for initializing the API with the compute engines
+	public ComputationalAPI(ComputeEngine computeEngine, ComputeEngineTwo computeEngineTwo) {
+		this.computeEngine = computeEngine;
+		this.computeEngineTwo = computeEngineTwo;
+	}
 
-    // Constructor for initializing the API with the compute engines
-    public ComputationalAPI(ComputeEngine computeEngine, ComputeEngineTwo computeEngineTwo) {
-        this.computeEngine = computeEngine;
-        this.computeEngineTwo = computeEngineTwo;
-    }
+	// Implementation of sending input to ComputeEngineTwo
+	public void sendInputToComputeEngineTwo(InputSource inputSource) {
+		System.out.println("Sending input to ComputeEngineTwo.");
 
-    // Implementation of sending input to ComputeEngineTwo
-    public void sendInputToComputeEngineTwo(InputSource inputSource) {
-        System.out.println("Sending input to ComputeEngineTwo.");
+		// Assuming inputSource provides a String
+		String inputData = (String) inputSource.getData();
+		if (inputData == null) {
+			System.out.println("Input data is empty.");
+			return;
+		}
 
-        // Assuming inputSource provides a String 
-        String inputData = (String) inputSource.getData();  
-        if (inputData == null) {
-            System.out.println("Input data is empty.");
-            return;
-        }
+		// Storing Result into Integer (since ComputeEngineTwo returns Integer)
+		Integer computedResult = (Integer) computeEngineTwo.performComputation(inputData);
 
-        // Storing Result into Integer (since ComputeEngineTwo returns Integer)
-        Integer computedResult = computeEngineTwo.performComputation(inputData);
+		// Sending result to OutputSource object
+		OutputSource<Integer> outputSource = new OutputSource<Integer>();
+		outputSource.setData(computedResult);
 
-        // Sending result to OutputSource object
-        OutputSource<Integer> outputSource = new OutputSource<Integer>();
-        outputSource.setData(computedResult);
+	}
 
-    }
-    
-    // Implementation for sending output to ComputeEngine
-    public void sendOutputToComputeEngine(OutputSource outputSource) {
-        System.out.println("Sending output to ComputeEngine.");
+	// Implementation for sending output to ComputeEngine
+	public void sendOutputToComputeEngine(OutputSource outputSource) {
+		System.out.println("Sending output to ComputeEngine.");
 
-        computeEngine.writeOutput("job#", outputSource);
-    }
- }
+		computeEngine.writeOutput("job#", outputSource);
+	}
+}

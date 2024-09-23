c1e8e859f764dcc50b6c49b238ef6ddf16d49f84
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-09-22 23:42:53 -0400

    Update ComputeEngineTwo.java

--- src/conceptual/api/ComputeEngineTwo.java
+++ src/conceptual/api/ComputeEngineTwo.java
@@ -2,8 +2,14 @@ package conceptual.api;
 
 public class ComputeEngineTwo implements ComputeEngineTwoInterface {
 
+	public Object performComputation(double[] inputData) {
+		// Implement actual computation logic here
+		return new Object();
+	}
+
+	@Override
 	public Object performComputation(Object inputData) {
-            //Implement actual computation logic here
-        return new Object();
-    }
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

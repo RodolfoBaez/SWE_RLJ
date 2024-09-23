9ce86093661e0e6195ff6621039182fc7efeceab
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-09-22 23:19:04 -0400

    Update ComputeEngine.java

--- src/conceptual/api/ComputeEngine.java
+++ src/conceptual/api/ComputeEngine.java
@@ -2,9 +2,9 @@ package conceptual.api;
 
 public class ComputeEngine implements ComputeEngineInterface {
 	public InputSource getInputSource(String jobId) {
-        //Implement logic to retrieve InputSource 
-        return new InputSource();
-    }
+		// Implement logic to retrieve InputSource
+		return new InputSource();
+	}
 
 	public void writeOutput(String jobId, OutputSource outputSource) {
 		// Implement logic to format and write the output from OutputSource

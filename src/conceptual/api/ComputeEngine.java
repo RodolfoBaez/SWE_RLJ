bfd31470e5d092dbfd8fb30523c141dcfe986e0c
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-10-07 22:54:44 -0400

    compile error
    does compute engine need that method?

--- src/conceptual/api/ComputeEngine.java
+++ src/conceptual/api/ComputeEngine.java
@@ -1,28 +1,32 @@
 package conceptual.api;
 
+import network.api.UserInput;
+
 public class ComputeEngine implements ComputeEngineInterface {
 
-    public InputSource getInputSource(String jobId) {
-        // Implement logic to retrieve InputSource
-        return new InputSource();
-    }
+	private int computedResultOutput; // Variable to store the computed result
 
-    private int computedResultOutput; // Variable to store the computed result
+	// method to return computed results to processAPI to send to data storage
+	public int[] getResults() {
+		int[] defaultArr = { 1, 3628800 };
+		return defaultArr;
+	}
 
-    // method to return computed results to processAPI to send to data storage
-    public int[] getResults() {
-        int[] defaultArr = { 1, 3628800 };
-        return defaultArr;
-    }
+	// Method to perform a computation and store the result
+	public void setOutput(int computedResult) {
+		// Store the computed result
+		this.computedResultOutput = computedResult;
+	}
 
-    // Method to perform a computation and store the result
-    public void setOutput(int computedResult) {
-        // Store the computed result
-        this.computedResultOutput = computedResult;
-    }
+	// Method to retrieve the last computed output
+	public int getOutput() {
+		return this.computedResultOutput; // Return the last computed result
+	}
 
-    // Method to retrieve the last computed output
-    public int getOutput() {
-        return this.computedResultOutput; // Return the last computed result
-    }
+	// TODO: implement logic to handle multiple jobs
+	@Override
+	public UserInput getInputSource(String jobId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

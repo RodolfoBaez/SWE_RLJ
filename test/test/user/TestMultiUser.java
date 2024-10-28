bb5e41b516c20c215aaa08483a26629c67f8e59c
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-10-28 09:53:50 -0400

    Checkstyle in MultiUser
    int var nThreads throwing checkstyle error in multiuser lines 42 and 82

--- test/test/user/TestMultiUser.java
+++ test/test/user/TestMultiUser.java
@@ -39,15 +39,15 @@ public class TestMultiUser {
 
 	@Test
 	public void compareMultiAndSingleThreaded() throws Exception {
-		int nThreads = 4;
+		int numberOfThreads = 4;
 		List<TestUser> testUsers = new ArrayList<>();
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numberOfThreads; i++) {
 			testUsers.add(new TestUser(coordinator));
 		}
 
 		// Run single threaded
 		String singleThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.singleThreadOut.tmp";
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numberOfThreads; i++) {
 			File singleThreadedOut = new File(singleThreadFilePrefix + i);
 			singleThreadedOut.deleteOnExit();
 			testUsers.get(i).run(singleThreadedOut.getCanonicalPath());
@@ -57,7 +57,7 @@ public class TestMultiUser {
 		ExecutorService threadPool = Executors.newCachedThreadPool();
 		List<Future<?>> results = new ArrayList<>();
 		String multiThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.multiThreadOut.tmp";
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numberOfThreads; i++) {
 			File multiThreadedOut = new File(multiThreadFilePrefix + i);
 			multiThreadedOut.deleteOnExit();
 			String multiThreadOutputPath = multiThreadedOut.getCanonicalPath();
@@ -74,8 +74,8 @@ public class TestMultiUser {
 		});
 
 		// Check that the output is the same for multi-threaded and single-threaded
-		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, nThreads);
-		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, nThreads);
+		List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, numberOfThreads);
+		List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, numberOfThreads);
 		Assert.assertEquals(singleThreaded, multiThreaded);
 	}
 

d9517d17322678b4807a27ba951b9dbd438b4c66
Joseph DiMino <59896424+DiMino-0@users.noreply.github.com>
2024-10-28 09:55:02 -0400

    Update TestMultiUser.java

--- test/test/user/TestMultiUser.java
+++ test/test/user/TestMultiUser.java
@@ -79,9 +79,9 @@ public class TestMultiUser {
 		Assert.assertEquals(singleThreaded, multiThreaded);
 	}
 
-	private List<String> loadAllOutput(String prefix, int nThreads) throws IOException {
+	private List<String> loadAllOutput(String prefix, int numberOfThreads) throws IOException {
 		List<String> result = new ArrayList<>();
-		for (int i = 0; i < nThreads; i++) {
+		for (int i = 0; i < numberOfThreads; i++) {
 			File multiThreadedOut = new File(prefix + i);
 			result.addAll(Files.readAllLines(multiThreadedOut.toPath()));
 		}
